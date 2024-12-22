a1fcd0e7aebd05316a66efbc0c0ddbb7afe06b66
Michal <michal@devx.agency>
2024-12-22 13:06:50 +0100

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const restoreBingo = JSON.parse(localStorage.getItem('bingo') || '[]')
+    const [imagesSet, setImagesSet] = React.useState<number[]>(Array(31).fill(1).map((_v, i) => i + 1).filter((image) => !restoreBingo.includes(image)));
+    const [bingoImages, setBingoImages] = React.useState<number[]>(restoreBingo);
+    const [bingoImage, setBingoImage] = React.useState<number>();
+    let interval: NodeJS.Timer | undefined
+
+    const findNearest = (num: number, arr: number[]): number => {
+        return arr.reduce((prev, curr) => Math.abs(curr - num) < Math.abs(prev - num) ? curr : prev);
+    };
+
+    window.onkeydown = (e: KeyboardEvent) => {
+        if (e.key === ' ' && !interval) {
+            if (window.confirm('Opravdu losovat?')) {
+                let i = 0
+                interval = setInterval(() => {
+                    const random = findNearest(Math.floor(Math.random() * 31) + 1, imagesSet);
+                    setBingoImage(random);
+                    i++;
+                    if (i > 25 && interval) {
+                        localStorage.setItem('bingo', JSON.stringify([...bingoImages, random]))
+                        setImagesSet(imagesSet.filter(image => image !== random));
+                        setBingoImages([...bingoImages, random]);
+                        clearInterval(interval);
+                        i = 0;
+                        interval = undefined
+                    }
+                }, 100)
+            }
+        }
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {bingoImage ?
+                    <img className="App-random-image" src={`/images/${bingoImage}.webp`} alt="big-bingo-image"/> :
+                    <samp className="App-question">?</samp>}
+                <div className="App-row"> {Array(31).fill(1).map((_, index) => (
+                    <div className="App-col">
+                        <img key={index} className="App-image"
+                             src={bingoImages[index] ? `/images/${bingoImages[index]}.webp` : `/question.png`}
+                             style={{opacity: bingoImages[index] ? 1 : 0}} alt="bingoimage"/>
+                    </div>
+                ))}
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
